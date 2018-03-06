26992846d7437c239e8b4850e2f4e071d165bf44
Farald <arkadiusz.placha@gmail.com>
2018-03-06 22:42:32 +0100

    Added gitignore + Clion project

---
+++ 1/charBlockArray/blockarray.h
@@ -0,0 +1,37 @@
+#ifndef blockarray_h
+#define blockarray_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "charblock.h"
+
+#define SUCCESS 0
+#define ERROR_OUT_OF_SPACE (-1)
+#define ERROR_NULL_POINTER (-2)
+#define ERROR_NOT_FOUND (-3)
+
+struct CharBlockArray {
+    unsigned int maxBlocksCount;
+    struct CharBlock **array;
+};
+
+struct CharBlockArray *create_array(unsigned int maxBlocks);
+
+void delete_array(struct CharBlockArray *blkArray);
+
+void delete_array_with_blocks(struct CharBlockArray *blkArray);
+
+int add_block(struct CharBlockArray *blkArray, struct CharBlock *block);
+
+int add_blocks(struct CharBlockArray *blkArray, struct CharBlock **blocks, unsigned int blocksCount);
+
+int remove_block(struct CharBlockArray *blkArray, struct CharBlock *block);
+
+int remove_blocks(struct CharBlockArray *blkArray, struct CharBlock **blocks, unsigned int blocksCount);
+
+unsigned int null_blocks_count(struct CharBlockArray *blkArray);
+
+struct CharBlock *find_nearest_sum_block(struct CharBlockArray *blkArray, int number);
+
+#endif /* blockarray_h */
