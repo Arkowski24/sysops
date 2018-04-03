ef6a13e7b91534aef6a0ee6fc65e03069a2e5941
Farald <arkadiusz.placha@gmail.com>
2018-04-03 18:10:32 +0200

    Filled header file + Changes in program logic(Uses child process for printing)

--- cw04/zad1/date_writer.c
+++ cw04/zad1/date_writer.c
@@ -9,34 +9,28 @@
 #include <signal.h>
 
 int continueExecution = 0;
-int childID = 0;
+int executorID = 0;
 
 void stop_signal_handler(int signal) {
-    if(continueExecution == 0){
+    kill(executorID, SIGINT);
+    if (continueExecution == 0) {
         fprintf(stdout, "Oczekuje na CTRL+Z - kontynuacja albo CTR+C - zakonczenie programu.\n");
         continueExecution = 1;
     } else {
+        create_executor();
         continueExecution = 0;
     }
-    return;
 }
 
 void interrupt_signal_handler(int signal) {
+    kill(executorID, SIGINT);
     fprintf(stdout, "Odebrano sygnal SIGINT.\n");
-    kill(childID, SIGINT);
     exit(EXIT_SUCCESS);
 }
 
-int main(int argc, char *argv[]) {
-    signal(SIGTSTP, stop_signal_handler);
-    signal(SIGINT, interrupt_signal_handler);
-
+void print_date() {
     while (1) {
-        if(continueExecution == 1){
-            pause();
-        }
-
-        childID = fork();
+        int childID = fork();
         if (childID == 0) {
             execlp("date", "-R", NULL);
         } else {
@@ -44,4 +38,35 @@ int main(int argc, char *argv[]) {
         }
         sleep(1);
     }
+}
+
+void create_executor() {
+    executorID = fork();
+    if (executorID == 0) {
+        signal(SIGTSTP, SIG_DFL);
+        signal(SIGINT, SIG_DFL);
+        print_date();
+    }
+}
+
+void set_interrupt_handler() {
+    signal(SIGINT, interrupt_signal_handler);
+}
+
+void set_stop_handler() {
+    struct sigaction signalAction;
+    signalAction.sa_handler = stop_signal_handler;
+    signalAction.sa_flags = 0;
+    signalAction.sa_mask = 0;
+    sigaction(SIGTSTP, &signalAction, NULL);
+}
+
+int main(int argc, char *argv[]) {
+    create_executor();
+    set_interrupt_handler();
+    set_stop_handler();
+    while (1) {
+        pause();
+    }
+    return 0;
 }
--- cw04/zad1/date_writer.h
+++ cw04/zad1/date_writer.h
@@ -5,4 +5,18 @@
 #ifndef SYSOPS_DATE_WRITER_H
 #define SYSOPS_DATE_WRITER_H
 
+void stop_signal_handler(int signal);
+
+void interrupt_signal_handler(int signal);
+
+void print_date();
+
+void create_executor();
+
+void set_interrupt_handler();
+
+void set_stop_handler();
+
+int main(int argc, char *argv[]);
+
 #endif //SYSOPS_DATE_WRITER_H
