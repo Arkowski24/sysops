a37f5d6515c8137e3438c8e358437ec885a026e0
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2018-04-06 00:08:11 +0200

    Basic error handling.

--- cw04/zad2/signal_tester.c
+++ cw04/zad2/signal_tester.c
@@ -14,11 +14,16 @@ extern unsigned long requestsToProceed;
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
+        fprintf(stderr, "%s\n", "Not enough arguments.");
         exit(EXIT_FAILURE);
     }
 
     createdChildren = strtoul(argv[1], NULL, 0);
     requestsToProceed = strtoul(argv[2], NULL, 0);
+    if (requestsToProceed > createdChildren) {
+        fprintf(stderr, "%s\n", "Approvals required cannot be higher than number of created children.");
+        exit(EXIT_FAILURE);
+    }
 
     parent_work();
     return 0;
