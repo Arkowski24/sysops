101bc84b73a08d4e0e1a230145f2cc78cb0a4500
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2018-04-06 14:07:34 +0200

    Added first part of the task.

---
+++ cw04/zad1/a/date_writer.c
@@ -0,0 +1,59 @@
+//
+// Created by Arkadiusz Placha on 02.04.2018.
+//
+
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "date_writer.h"
+#include <sys/time.h>
+#include <time.h>
+
+int continueExecution = 0;
+
+void stop_signal_handler(int signal) {
+    if (continueExecution == 0) {
+        fprintf(stdout, "Oczekuje na CTRL+Z - kontynuacja albo CTR+C - zakonczenie programu.\n");
+        continueExecution = 1;
+    } else {
+        continueExecution = 0;
+    }
+}
+
+void interrupt_signal_handler(int signal) {
+    fprintf(stdout, "Odebrano sygnal SIGINT.\n");
+    exit(EXIT_SUCCESS);
+}
+
+void print_date() {
+    time_t rawTime;
+    time(&rawTime);
+    printf("%s", ctime(&rawTime));
+}
+
+void set_interrupt_handler() {
+    signal(SIGINT, interrupt_signal_handler);
+}
+
+void set_stop_handler() {
+    struct sigaction signalAction;
+    sigset_t newMask;
+    sigemptyset(&newMask);
+
+    signalAction.sa_handler = stop_signal_handler;
+    signalAction.sa_flags = 0;
+    signalAction.sa_mask = newMask;
+    sigaction(SIGTSTP, &signalAction, NULL);
+}
+
+int main(int argc, char *argv[]) {
+    set_interrupt_handler();
+    set_stop_handler();
+    while (1) {
+        print_date();
+        sleep(1);
+    }
+
+    return 0;
+}
---
+++ cw04/zad1/a/date_writer.h
@@ -0,0 +1,16 @@
+//
+// Created by Arkadiusz Placha on 02.04.2018.
+//
+
+#ifndef SYSOPS_DATE_WRITER_H
+#define SYSOPS_DATE_WRITER_H
+
+void print_date();
+
+void set_interrupt_handler();
+
+void set_stop_handler();
+
+int main(int argc, char *argv[]);
+
+#endif //SYSOPS_DATE_WRITER_H
