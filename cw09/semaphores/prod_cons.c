dab32cb84fa6d99351c5435aa17c37a5217f301e
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2018-06-05 21:09:04 +0200

    Added base for second version.

---
+++ cw09/semaphores/prod_cons.c
@@ -0,0 +1,141 @@
+//
+// Created by Arkadiusz Placha on 31.05.18.
+//
+
+#include "prod_cons.h"
+#include <pthread.h>
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/times.h>
+#include "../fifo/circular_fifo.h"
+
+#define MAX_STRING_LEN 256
+
+CircularFifo_t *fifo;
+FILE *file;
+
+pthread_t *producersID;
+pthread_t *consumersID;
+unsigned int producersCount;
+unsigned int consumersCount;
+
+int compare_mode;
+unsigned int searched_length;
+unsigned int verbose;
+unsigned int timeout;
+
+pthread_mutex_t mutex;
+pthread_cond_t empty;
+pthread_cond_t full;
+
+void parse_arguments(char *argv[]) {
+    producersCount = (unsigned int) strtoul(argv[1], NULL, 0);
+    consumersCount = (unsigned int) strtoul(argv[2], NULL, 0);
+    searched_length = (unsigned int) strtoul(argv[5], NULL, 0);
+
+    if(producersCount == 0 || consumersCount == 0) {
+        printf("Number of producers and consumers must be more than 0.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    compare_mode = (int) strtol(argv[6], NULL, 0);
+    verbose = (unsigned int) strtoul(argv[7], NULL, 0);
+    timeout = (unsigned int) strtoul(argv[8], NULL, 0);
+}
+
+FILE *open_file(char *filename) {
+    FILE * file = fopen(filename, "r");
+    if(file == NULL) {
+        perror("Open file error.");
+        exit(EXIT_FAILURE);
+    }
+    return file;
+}
+
+void setup(char *argv[]) {
+    parse_arguments(argv);
+    file = open_file(argv[4]);
+
+    unsigned int queueSize = (unsigned int) strtoul(argv[3], NULL, 0);
+    if(queueSize == 0) {
+        printf("Queue size must be more than 0.\n");
+        exit(EXIT_FAILURE);
+    }
+    initialize_queue(queueSize);
+    initialize_mutex();
+}
+
+void start_thread(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg) {
+    if (pthread_create(thread, attr, start_routine, arg)) {
+        perror("Thread creation error.");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void dispatch_tasks() {
+    assert(producersCount > 0);
+    assert(consumersCount > 0);
+
+    producersID = calloc(producersCount, sizeof(pthread_t));
+    consumersID = calloc(consumersCount, sizeof(pthread_t));
+
+    for (int i = 0; i < producersCount; ++i) {
+        start_thread(producersID + i, NULL, producer_routine, producersID + i);
+    }
+    for (int i = 0; i < consumersCount; ++i) {
+        start_thread(consumersID + i, NULL, consumer_routine, consumersID + i);
+    }
+}
+
+void print_kill_message() {
+    printf("%ld: Thread %ld is cancelling other threads.\n", times(NULL), pthread_self());
+}
+
+void print_timeout_message() {
+    printf("%ld: Maximum time reached. \n", times(NULL));
+}
+
+void kill_other_threads() {
+    print_kill_message();
+    pthread_t caller = pthread_self();
+    for (int i = 0; i < producersCount; ++i) {
+        if (producersID[i] != caller)
+            pthread_cancel(producersID[i]);
+    }
+    for (int i = 0; i < consumersCount; ++i) {
+        if (consumersID[i] != caller)
+            pthread_cancel(consumersID[i]);
+    }
+    pthread_cond_broadcast(&full);
+    pthread_cond_broadcast(&empty);
+
+}
+
+void collect_threads() {
+    for (int i = 0; i < producersCount; ++i) {
+        pthread_join(producersID[i], NULL);
+    }
+    for (int i = 0; i < consumersCount; ++i) {
+        pthread_join(consumersID[i], NULL);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if(argc < 9) {
+        printf("Not enough arguments.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    setup(argv);
+    dispatch_tasks();
+
+    if (timeout > 0) {
+        sleep(timeout);
+        if (verbose) { print_timeout_message(); }
+        kill_other_threads();
+    }
+    collect_threads();
+
+    return 0;
+}
---
+++ cw09/semaphores/prod_cons_rout.c
@@ -0,0 +1,174 @@
+//
+// Created by farald on 01.06.18.
+//
+#include "prod_cons.h"
+#include <pthread.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/times.h>
+#include "../fifo/circular_fifo.h"
+
+#define MAX_STRING_LEN 256
+
+CircularFifo_t *fifo;
+extern FILE *file;
+
+extern int compare_mode;
+extern int searched_length;
+extern unsigned int verbose;
+extern unsigned int timeout;
+
+pthread_mutex_t mutex;
+pthread_cond_t empty;
+pthread_cond_t full;
+
+
+void print_acquire_mutex_message() {
+    printf("%ld: Thread %ld locked mutex.\n", times(NULL), pthread_self());
+}
+
+void print_released_mutex_message() {
+    printf("%ld: Thread %ld unlocked mutex.\n", times(NULL), pthread_self());
+}
+
+void print_thread_finished_message() {
+    printf("%ld: Thread %ld finished. \n", times(NULL), pthread_self());
+}
+
+void print_inserted_message() {
+    printf("%ld: Thread %ld inserted element into buffer. \n", times(NULL), pthread_self());
+}
+
+void print_retrieved_message() {
+    printf("%ld: Thread %ld retrieved element from buffer. \n", times(NULL), pthread_self());
+}
+
+void print_eof_message() {
+    printf("%ld: Thread %ld reached end of file. \n", times(NULL), pthread_self());
+}
+
+void cleanup_handler(void *arg) {
+    pthread_mutex_unlock(&mutex);
+    if (verbose) {
+        print_released_mutex_message();
+        print_thread_finished_message();
+    }
+}
+
+void initialize_queue(unsigned int qSize) {
+    size_t memSize = offsetof(CircularFifo_t, queue) + sizeof(char *) * qSize;
+    fifo = malloc(memSize);
+    fifo_initialize(fifo, qSize);
+}
+
+void initialize_mutex() {
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&empty, NULL);
+    pthread_cond_init(&full, NULL);
+}
+
+int my_strlen(const char *str) {
+    int size = 0;
+    while (*(str + size) != '\0' && *(str + size) != '\n') {
+        size++;
+    }
+    return size;
+}
+
+void *producer_routine(void *arg) {
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+    while (1) {
+        pthread_mutex_lock(&mutex);
+        if (verbose) { print_acquire_mutex_message(); }
+        pthread_cleanup_push(cleanup_handler, NULL) ;
+
+                pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+                while (fifo_full(fifo)) {
+                    pthread_cond_wait(&full, &mutex);
+                }
+                pthread_testcancel();
+
+                char *string = calloc(sizeof(char), MAX_STRING_LEN);
+                if (fgets(string, MAX_STRING_LEN, file) == NULL) {
+                    if (verbose && feof(file)) { print_eof_message(); }
+                    free(string);
+
+                    if (timeout == 0) { kill_other_threads(); }
+                    pthread_exit(NULL);
+                }
+
+                fifo_push(fifo, string);
+                if (verbose) { print_inserted_message(); }
+
+                pthread_cond_broadcast(&empty);
+
+                pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+
+        pthread_cleanup_pop(0);
+        pthread_mutex_unlock(&mutex);
+        if (verbose) { print_released_mutex_message(); }
+    }
+}
+
+int is_valid_string(char *str) {
+    int elemLength = my_strlen(str);
+    int print;
+
+    switch (compare_mode) {
+        case -1:
+            print = elemLength < searched_length;
+            break;
+        case 0:
+            print = elemLength == searched_length;
+            break;
+        case 1:
+            print = elemLength > searched_length;
+            break;
+        default:
+            print = 0;
+    }
+
+    return print;
+}
+
+void print_valid_string(int index, char *str) {
+    if (verbose) { printf("%ld: Thread %ld found valid string. ", times(NULL), pthread_self()); }
+    printf("Index: %3d, String: %s", index, str);
+}
+
+void *consumer_routine(void *arg) {
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+    int qIndex;
+    char *elem;
+
+    while (1) {
+        pthread_mutex_lock(&mutex);
+        if (verbose) { print_acquire_mutex_message(); }
+        pthread_cleanup_push(cleanup_handler, NULL) ;
+
+                pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+                while (fifo_empty(fifo)) {
+                    pthread_cond_wait(&empty, &mutex);
+                }
+                pthread_testcancel();
+
+                qIndex = (int) fifo->readItr;
+                elem = fifo_pop(fifo);
+                if (verbose) { print_retrieved_message(); }
+
+                pthread_cond_broadcast(&full);
+
+                pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+
+        pthread_cleanup_pop(0);
+        pthread_mutex_unlock(&mutex);
+        if (verbose) { print_released_mutex_message(); }
+
+        if (is_valid_string(elem)) {
+            print_valid_string(qIndex, elem);
+        }
+        free(elem);
+    }
+}
